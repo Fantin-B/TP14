a657ad787e091904f79d3118a924130e6927736b
Fantin <fantin.brisard@isen-ouest.yncrea.fr>
2020-05-27 14:00:33 +0200

    TP fini

--- ex2.py
+++ ex2.py
@@ -8,21 +8,15 @@ class Window2(QWidget):
         self.setWindowTitle("Laisser un commentaire")
         self.layout = QGridLayout()
 
-        self.label = QLabel("""
-        
-        
-        
-        a great comment
-        
-        
-        
-        """)
+        self.label = QLabel("Laissez un commentaire")
+        self.txt = QTextEdit()
         self.button1 = QPushButton("Success")
         self.button2 = QPushButton("Cancel")
 
-        self.layout.addWidget(self.label,0,0)
-        self.layout.addWidget(self.button1,1,0)
-        self.layout.addWidget(self.button2,1,1)
+        self.layout.addWidget(self.label,0,0,1,2)
+        self.layout.addWidget(self.txt,1,0,1,2)
+        self.layout.addWidget(self.button1,2,0,1,1)
+        self.layout.addWidget(self.button2,2,1,1,1)
 
         self.setLayout(self.layout)
 
